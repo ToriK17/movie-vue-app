f6bcb942e4cac2f8c4db5205563c4e52a0f422bf
ToriK17 <vfcad@protonmail.com>
2019-10-14 14:57:44 -0700

    adds nav bar and bootstrap template

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,102 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">All Actors</router-link> |
-      <router-link to="/about">About</router-link> | 
-      <router-link to="/movies/new">New Movie</router-link> |
-      <router-link to="/movies">All Movies</router-link>  
+    <div class="cbp-af-header">
+      <div class=" cbp-af-inner">
+        <div class="container">
+          <div class="row">
+
+            <div class="span4">
+              <!-- logo -->
+              <div class="logo">
+                <h1><a href="index.html">Movie Spot</a></h1>
+                <!-- <img src="assets/img/logo.png" alt="" /> -->
+              </div>
+              <!-- end logo -->
+            </div>
+
+            <div class="span8">
+              <!-- top menu -->
+              <div class="navbar">
+                <div class="navbar-inner">
+                  <nav>
+                    <ul class="nav topnav">
+                      <li class="dropdown active">
+                        <a href="index.html">Home </a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#">Movies</a>
+                        <ul class="dropdown-menu">
+                          <li><router-link to="/movies">All Movies</router-link></li>
+                          <li><router-link to="/movies/new">New Movie</router-link></li>
+                        </ul>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#">Actors</a>
+                        <ul class="dropdown-menu">
+                          <li><router-link to="/">All Actors</router-link></li>
+                        </ul>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#">About</a>
+                        <ul class="dropdown-menu">
+                          <li><<router-link to="/about">About</router-link></li>
+                        </ul>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#">Blog</a>
+                        <ul class="dropdown-menu">
+                          <li><a href="blog_right_sidebar.html">Blog right sidebar</a></li>
+                          <li><a href="post_left_sidebar.html">Post left sidebar</a></li>
+                          <li><a href="post_right_sidebar.html">Post right sidebar</a></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="contact.html">Contact</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+              <!-- end menu -->
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
+
+
+
+
+
+<!-- <div id="nav">
+      <router-link to="/">All Actors</router-link> |
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/movies/new">New Movie</router-link> |
+      <router-link to="/movies">All Movies</router-link>  
+    </div> -->
